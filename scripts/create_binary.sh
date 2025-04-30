bec653c76bccd2535c9ee1b517f16482cf894f82
FourierTransformer <FourierTransformer@users.noreply.github.com>
2025-04-30 17:53:36 -0500

    stripping back some stuff

--- scripts/create_binary.sh
+++ scripts/create_binary.sh
@@ -4,18 +4,18 @@
 
 set -e
 
-python3 -m pip install hererocks
+pip install hererocks
 hererocks -l "@v5.4.7" -r "@v3.11.1" tls
 
 source tls/bin/activate
 luarocks install teal-language-server
-rm ./tls/bin/activate* ./tls/bin/get_deactivated_path.lua
-rm ./tls/bin/tree-sitter ./tls/bin/json2lua ./tls/bin/lua2json
-rm ./tls/bin/luarocks ./tls/bin/luarocks-admin ./tls/bin/tl
+#rm ./tls/bin/activate* ./tls/bin/get_deactivated_path.lua
+#rm ./tls/bin/tree-sitter ./tls/bin/json2lua ./tls/bin/lua2json
+#rm ./tls/bin/luarocks ./tls/bin/luarocks-admin ./tls/bin/tl
 
-tls_dir="$(pwd)/tls/"
-sed -i '' -e '2i\
-cd "$(dirname "$0")"' ./tls/bin/teal-language-server
-sed -i '' -e "s*$tls_dir*../*g" ./tls/bin/teal-language-server
+#tls_dir="$(pwd)/tls/"
+#sed -i '' -e '2i\
+#cd "$(dirname "$0")"' ./tls/bin/teal-language-server
+#sed -i '' -e "s*$tls_dir*../*g" ./tls/bin/teal-language-server
 
 teal-language-server --help
